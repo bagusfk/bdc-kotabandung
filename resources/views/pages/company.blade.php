36bd410d32c122c5453c85ae327adfdc1e88e003
Zuryu99 <masdadi0896@gmail.com>
2024-03-21 22:03:49 +0700

    responsive style

--- resources/views/pages/company.blade.php
+++ resources/views/pages/company.blade.php
@@ -6,25 +6,25 @@
 
     <div class="pt-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="grid grid-cols-2 gap-4 bg-zinc-200">
-                <div class="pl-[2rem] pt-[2rem]">
+            <div class="grid md:grid-cols-2 gap-4 bg-zinc-200">
+                <div class="px-[2rem] py-[2rem]">
                     <h2 class="text-[2rem] font-medium">Profile Perusahaan</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quae quaerat nulla, ducimus dolorem sapiente recusandae quos, repellat animi officiis perspiciatis rerum saepe quis tempore, quidem ratione! Doloremque, magni ipsum.Eligendi, amet. Exercitationem rerum optio quod tempore omnis quasi alias amet repudiandae! Maiores dolorem veritatis deserunt eaque facere dolores distinctio vel fuga? Ullam, sapiente. Consectetur cumque sed ea adipisci blanditiis.</p>
                 </div>
                 <div>
-                    <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
+                    <img class="w-full h-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
                 </div>
             </div>
 
             <section class="py-[2.5rem]">
                 <div class="flex w-full justify-center py-3">
-                    <div class="w-1/2">
+                    <div class="lg:w-1/2 max-lg:px-[2rem]">
                         <h2 class="text-[2rem] font-medium text-center">VISI</h2>
                         <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt esse provident repellendus explicabo, eveniet in maiores quod ratione voluptas, iure quaerat et blanditiis ipsam iste laudantium dignissimos consectetur sit molestiae?Minus ratione excepturi eaque hic at atque dolor, suscipit iusto quidem odio eos exercitationem doloribus tempora laboriosam earum, aliquid ut beatae incidunt quam deserunt maiores ipsum velit. Accusantium, laboriosam dolorem.</p>
                     </div>
                 </div>
                 <div class="flex w-full justify-center">
-                    <div class="w-1/2">
+                    <div class="lg:w-1/2 max-lg:px-[2rem]">
                         <h2 class="text-[2rem] font-medium text-center">MISI</h2>
                         <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt esse provident repellendus explicabo, eveniet in maiores quod ratione voluptas, iure quaerat et blanditiis ipsam iste laudantium dignissimos consectetur sit molestiae?Minus ratione excepturi eaque hic at atque dolor, suscipit iusto quidem odio eos exercitationem doloribus tempora laboriosam earum, aliquid ut beatae incidunt quam deserunt maiores ipsum velit. Accusantium, laboriosam dolorem.</p>
                     </div>
@@ -43,10 +43,10 @@
                     </div>
                 </div>
             </section>
-            <section class="py-[2.5rem] pb-[16rem]">
+            <section class="py-[2.5rem] pb-[16rem] max-lg:px-[2rem]">
                 <h2 class="text-[2rem] font-medium">Perkembangan dan Fungsi BDC</h2>
                 <br>
-                <div class="grid grid-cols-2 gap-4">
+                <div class="grid md:grid-cols-2 gap-4">
                     <div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quos, pariatur fugiat earum quasi laudantium vero assumenda voluptatum minus! Ipsa officia quo veniam in voluptas expedita illum. Dolore, tempora et.</p>
                     </div>
@@ -61,25 +61,23 @@
                     </div>
                 </div>
             </section>
-            <section class="pb-[10rem] pl-[3rem] bg-zinc-200">
-                <div class="grid grid-cols-3">
+            <section class="py-[7rem] px-[3rem] bg-zinc-200">
+                <div class="grid max-md:grid-cols-1 lg:grid-cols-3 max-lg:grid-cols-2 gap-4">
                     <div class="my-4 items-start flex">
                         <img class="w-10" src="{{ asset('assets/location-dot-solid.svg') }}" alt="">
-                        <span class="ml-[2rem]">Jl. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta nesciunt, magni eligendi eum enim, dicta totam, vero dolorem illo unde fugit mollitia reiciendis quaerat culpa. Quae illum nisi nihil aut.</span>
+                        <span class="ml-4">Jl. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta nesciunt, magni eligendi eum enim, dicta totam, vero dolorem illo unde fugit mollitia reiciendis quaerat culpa. Quae illum nisi nihil aut.</span>
                     </div>
-                    <div>
-                        <ul class="my-4 pl-[8rem]">
-                            <li>Company</li>
-                            <li>E-Catalog</li>
-                            <li>Event</li>
-                        </ul>
+                    <div class="my-4 w-full">
+                        <a href="{{route('company_profile')}}" class="w-full inline-block pl-[3.5rem] hover:underline max-md:py-2">Company</a>
+                        <a href="#" class="w-full inline-block pl-[3.5rem] hover:underline pt-2">E-Catalog</a>
+                        <a href="#" class="w-full inline-block pl-[3.5rem] hover:underline pt-2">Event</a>
                     </div>
-                    <div>
-                        <div class="flex items-center h-10 my-4 pl-[5rem]">
+                    <div class="my-4">
+                        <div class="flex items-center h-10 lg:pl-[5rem]">
                             <img class="w-10" src="{{ asset('assets/instagram.svg') }}" alt="">
                             <span class="align-middle ml-4">@bdjbandungjuara</span>
                         </div>
-                        <div class="flex items-center h-10 pl-[5rem]">
+                        <div class="flex items-center h-10 my-4 lg:pl-[5rem]">
                             <img class="w-10" src="{{ asset('assets/whatsapp.svg') }}" alt="">
                             <span class="align-middle ml-4">0812-2254-1485</span>
                         </div>
