a5fed500c889d0631856d0dfd21c42517f04dcc9
Bagus Farhan Kirana <bagusfk08@gmail.com>
2024-04-18 14:16:02 +0700

    Update AuthenticatedSessionController.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -60,7 +60,7 @@ class AuthenticatedSessionController extends Controller
         Auth::guard('web')->logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('/dashboard');
+        return redirect('/');
     } else {
         Auth::guard('web')->logout();
         $request->session()->invalidate();
