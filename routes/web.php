31dccb9d3a2e44b64b1659927c84fcef77fa46b1
Bagus Farhan Kirana <bagusfk08@gmail.com>
2024-04-19 08:13:38 +0700

    logout handle

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/e-catalog/{id}', [StokbarangController::class, 'detail'])->name('de
 // })->name('dashboard');
 // ->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth','revalidate'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
