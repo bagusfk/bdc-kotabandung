49d6c93918f7c7331b1a19a1a482c57080b2aa9b
BagusFk <97009227+bagusfk@users.noreply.github.com>
2024-09-14 12:40:47 +0700

    fix

--- resources/views/pages/kepalabagian/barang/view.blade.php
+++ resources/views/pages/kepalabagian/barang/view.blade.php
@@ -3,18 +3,18 @@
 @section('content')
     <div class="flex justify-between">
         <div class="flex items-center">
-            <svg class="w-6 h-6 inline-flex" xmlns="http://www.w3.org/2000/svg"
+            <svg class="inline-flex h-6 w-6" xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                 <path
                     d="M184 48H328c4.4 0 8 3.6 8 8V96H176V56c0-4.4 3.6-8 8-8zm-56 8V96H64C28.7 96 0 124.7 0 160v96H192 320 512V160c0-35.3-28.7-64-64-64H384V56c0-30.9-25.1-56-56-56H184c-30.9 0-56 25.1-56 56zM512 288H320v32c0 17.7-14.3 32-32 32H224c-17.7 0-32-14.3-32-32V288H0V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V288z" />
             </svg>
-            <span class="text-2xl font-semibold ml-[1rem]">Kelola Barang</span>
+            <span class="ml-[1rem] text-2xl font-semibold">Laporan Barang</span>
         </div>
     </div>
 
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg my-[1rem]">
-        <table id="dataTable" class="display nowrap w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <div class="relative my-[1rem] overflow-x-auto shadow-md sm:rounded-lg">
+        <table id="dataTable" class="display nowrap w-full text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400">
+            <thead class="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" class="px-6 py-3">
                         No
@@ -48,7 +48,7 @@
                 @endphp
                 @foreach ($items as $data)
                     <tr>
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
                             {{ $no++ }}
                         </th>
                         <td class="px-6 py-4">
